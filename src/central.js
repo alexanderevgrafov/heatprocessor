bebb6e66f34707f02a9a894ebdfb2de8c16e05fd
Vlad Balin <gaperton@gmail.com>
2018-06-29 12:15:51 -0400

    Fixed bugs

--- src/central.js
+++ src/central.js
@@ -1,25 +1,28 @@
-import { Record } from 'type-r'
+require('source-map-support').install();
+
+import { define, Record } from 'type-r'
 import socketio from 'socket.io'
 
 require('dotenv').config();
 
-class State extends Record`` {
-    static smth = "The answer is 42";
-
-    static system =  Record.defaults({
-        mode: 0,
-        airtower: Record.defaults({
-            t: 0,
-            mode: 0
+@define
+class State extends Record {
+    static attributes = {
+        smth : "The answer is 42",
+        system :  Record.defaults({
+            mode: 0,
+            airtower: Record.defaults({
+                t: 0,
+                mode: 0
+            })
+        }),
+        settings : Record.defaults({
+            price: Record.defaults({
+                electricity_day: 25,
+                electricity_night: 12
+            })
         })
-    });
-
-    static settings = Record.defaults({
-        price: Record.defaults({
-            electricity_day: 25,
-            electricity_night: 12
-        })
-    });
+    }
 }
 
 const state = new State(),
