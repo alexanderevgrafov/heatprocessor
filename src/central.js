9d7ea7267fc798ab4e696b83977359f45f706fd1
alexanderevgrafov <alexander.evgrafov@gmail.com>
2018-06-30 21:08:47 +0300

    Added state saving on server stop.  However, still not working under nodemon

--- src/central.js
+++ src/central.js
@@ -1,27 +1,50 @@
-require('source-map-support').install();
+require( 'source-map-support' ).install();
 import * as socketio from 'socket.io'
 import { SystemState } from "./SystemState.js";
+import * as fs from "fs"
 
-require('dotenv').config();
+require( 'dotenv' ).config();
+
+const SAVED_STATE_FILE = './saved_state.json';
 
 const state = new SystemState(),
-    say = console.log,
-    io = socketio.listen(process.env.central_server_port);
+      say   = console.log,
+      io    = socketio.listen( process.env.central_server_port );
 
-io.on('connection', socket => {
-    socket.on('disconnect', () => say('WS client is disconnected'));
-    say('WS client is connected');
+try{
+    state.set( JSON.parse( fs.readFileSync( SAVED_STATE_FILE ) ) );
+    console.log( 'State restored' );
+}
+catch( err ){
+    console.log( 'Failed to restore a state....' );
+}
 
-    socket.emit('news', {hello: 'world'});
+io.on( 'connection', socket =>{
+    socket.on( 'disconnect', () => say( 'WS client is disconnected' ) );
+    say( 'WS client is connected' );
 
-    socket.on('hola', data => {
-        say('Hola from', data);
-        io.sockets.emit('sysupdate', state.toJSON());
-    });
+    socket.emit( 'news', { hello : 'world' } );
 
-    socket.on('settings', data => {
-        state.set(data);
-        io.sockets.emit('sysupdate', state.toJSON());
-    })
+    socket.on( 'hola', data =>{
+        say( 'Hola from', data );
+        io.sockets.emit( 'sysupdate', state.toJSON() );
+    } );
 
-});
+    socket.on( 'settings', data =>{
+        state.set( data );
+        io.sockets.emit( 'sysupdate', state.toJSON() );
+    } )
+
+} );
+
+const grasefulExit = () =>{
+    console.log('---------');
+
+    const text = JSON.stringify( state.toJSON() );
+    fs.writeFileSync( SAVED_STATE_FILE, text );
+
+    console.log( "Terminating...", text );
+    process.abort();
+};
+
+process.on( 'SIGINT', grasefulExit );
